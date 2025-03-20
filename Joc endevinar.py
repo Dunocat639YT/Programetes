4d1cb5fb3db44dd30c2a31c0ef4adca6149d9680
Dunocat639YT <francescpasto@gmail.com>
2025-03-20 14:31:00 +0100

    ukraine

--- Joc endevinar.py
+++ Joc endevinar.py
@@ -15,13 +15,13 @@ RESET = "\033[0m"
 
 # Configurar salutació en funció de l'hora
 if hora_actual < 12:
-    hora = ("Bon dia")
+    hora = ("доброго ранку")
 elif hora_actual < 18:
-    hora = ("Bona tarda")
+    hora = ("доброго дня")
 elif hora_actual < 21:
-    hora = ("Bona vesprada")
+    hora = ("доброго дня")
 else:
-    hora = ("Bona nit")
+    hora = ("на добраніч")
 
 # Comença el joc
 def joc_endevina():
@@ -29,33 +29,33 @@ def joc_endevina():
     intents = 0
     
     #Salutació
-    print(f"{YELLOW}{hora}, benvingut al joc! He pensat en un número de l'1 al 100. A veure si adivines quin!{RESET}")
+    print(f"{YELLOW}{hora}, Ласкаво просимо до гри! Я придумав число від 1 до 100. Давайте перевіримо, чи вгадаєте ви, яке саме!{RESET}")
     time.sleep(2)
     print(" ")
 
     while True: 
         try: 
             # Pregunta el número
-            endevina = int(input(f"{GREEN}Quin número creus que he pensat?{RESET} "))
+            endevina = int(input(f"{GREEN}Як ви думаєте, яке число я придумав?{RESET} "))
             intents += 1
             print(" ")
 
             # Comprova si el número és correcte i dona una pista l'usuari
             if endevina > numero_secret:
-                print(f"{RED}El número que he pensat és més petit!{RESET}")
+                print(f"{RED}Число, про яке я думав, менше!{RESET}")
                 time.sleep(1)
                 print(" ")
             elif endevina < numero_secret:
-                print(f"{RED}El número que he pensat és més gran!{RESET}")
+                print(f"{RED}Число, про яке я думав, більше!{RESET}")
                 time.sleep(1)
                 print(" ")
             else:
-                print(f"{YELLOW}Felicitats! Has endevinat el número{RESET} {PURPLE}{numero_secret}{RESET} {YELLOW}en{RESET} {PURPLE}{intents}{RESET} intents")
+                print(f"{YELLOW}Щиро вітаю! Ви вгадали номер{RESET} {PURPLE}{numero_secret}{RESET} {YELLOW}із{RESET} {PURPLE}{intents}{RESET} спроб.")
                 break
         # Si l'usuari és un tros de quòniam i no introdueix un número
         except ValueError:
             print(" ")
-            print(f"{RED} A veure petit tros de quòniam, has d'introduir un número enter!{RESET}")
+            print(f"{RED} Давайте подивимося маленький шматочок quòniam, вам потрібно ввести ціле число!{RESET}")
             time.sleep(2)
             print(" ")
 
