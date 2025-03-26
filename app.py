9de8cc862411147b09a354fd04f6089481c68bd2
Dunocat639YT <166832596+Dunocat639YT@users.noreply.github.com>
2025-03-27 00:02:46 +0100

    try i except posadet

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-nom = input("Com et dius preciós/preciosa/preciose? ")
-estat = input(f"Hola, {nom}, com estàs?" ).strip().lower()
+import time
+
+try:
+    nom = input("Com et dius preciós/preciosa/preciose? ")
+except:
+    print("A veure tros de quòniam, tu et dius número o que?")
+finally:
+    estat = input(f"Hola, {nom}, com estàs?" ).strip().lower()
+
 if estat == "bé":
     print("M'alegro molt!")
 else: 
     print(f"Vaja, ho sento molt, {nom}.")
-x= input("Dis-me un numero aleatori ")
-y= input("Ara dis-me un altre numero aleatori ")
+
+try:
+    x = input("Dis-me un numero aleatori ")
+    y = input("Ara dis-me un altre numero aleatori ")
+except:
+    print("Sembla que la inteligència no és lo teu.
+    time.sleep(1)
+    print("He dit un número.")
+    time.sleep(2)
+    print("Tros.")
+    time.sleep(1)
+    print("De.")
+    time.sleep(1)
+    print("Quòniam.")
+    time.sleep(1)
 
 try:
     print(f"La suma dels dos numeros aleatoris que m'has donat és {int(x)+int(y)}")
