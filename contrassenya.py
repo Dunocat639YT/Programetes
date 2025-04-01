488f0f058d89dbc60b3df00113ddb3043f739146
Dunocat639YT <francescpasto@gmail.com>
2025-04-01 19:17:11 +0200

    contrasenya.py

---
+++ contrassenya.py
@@ -0,0 +1,32 @@
+import time
+
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+PURPLE = "\033[95m"
+RESET = "\033[0m"
+
+contassenya_guardada = "Gatets123"
+
+def programa():
+    contrassenya_adivinada = False
+
+    while not contrassenya_adivinada:
+        print()
+        contrassenya_posada = input(f"{YELLOW}Introdueix la contrassenya: {RESET}")
+
+        if contrassenya_posada == contassenya_guardada:
+          contrassenya_adivinada = True
+          print()
+          print(f"{GREEN}La contrassenya és correcta!{RESET}")
+          time.sleep(1)
+          print()
+          input(f"{PURPLE}Prem 'Enter' per continuar...{RESET}")
+          break
+        else:
+            print()
+            print(f"{RED}La contrassenya és incorrecta!{RESET}")
+
+while True:
+    programa()
