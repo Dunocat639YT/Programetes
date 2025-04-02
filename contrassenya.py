55bacebee6a9fa6788c5c167057a5ad344c5e0eb
Dunocat639YT <francescpasto@gmail.com>
2025-04-02 09:15:53 +0200

    Sistema canviar contrassenya

--- contrassenya.py
+++ contrassenya.py
@@ -7,16 +7,18 @@ BLUE = "\033[94m"
 PURPLE = "\033[95m"
 RESET = "\033[0m"
 
-contassenya_guardada = open("jejeje1.txt", "r").read()
+
 
 def programa():
     contrassenya_adivinada = False
+    intents = 0
+    contrassenya_guardada = open("contrassenya1.txt", "r").read()
 
     while not contrassenya_adivinada:
         print()
         contrassenya_posada = input(f"{YELLOW}Introdueix la contrassenya: {RESET}")
 
-        if contrassenya_posada == contassenya_guardada:
+        if contrassenya_posada == contrassenya_guardada:
           contrassenya_adivinada = True
           print()
           print(f"{GREEN}La contrassenya és correcta!{RESET}")
@@ -27,6 +29,23 @@ def programa():
         else:
             print()
             print(f"{RED}La contrassenya és incorrecta!{RESET}")
-
+            intents += 1
+        if intents == 3:
+            print()
+            canviar = input(f"{RED}Has oblidat la contrassenya? Si vols, la pots canviar. Y/N?{RESET}")
+            if canviar.lower() == "y":
+                print()
+                nova_contrassenya = input(f"{YELLOW}Introdueix la nova contrassenya: {RESET}")
+                with open("contrassenya1.txt", "w") as arxiu:
+                    arxiu.write(nova_contrassenya)
+                contrassenya_guardada = open("contrassenya1.txt", "r").read()
+                print()
+                print(f"{GREEN}La contrassenya s'ha canviat correctament!{RESET}")
+                time.sleep(1)
+                intents = 0
+            else:
+                print()
+                print(f"{YELLOW}D'acord{RESET}")
+                intents = 0
 while True:
     programa()
