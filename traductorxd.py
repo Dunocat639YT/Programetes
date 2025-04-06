02b4ff487b03ad25f509d66ced2bed5cd0b83805
Dunocat639YT <francescpasto@gmail.com>
2025-04-06 19:42:31 +0200

    .

---
+++ traductorxd.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import tkk
+from deep_translator import GoogleTranslator
+
+idiomes ={
+    "Català": "ca",
+    "Castellà": "es",
+    "Anglès": "en",
+    "Francès": "fr",
+    "Alemany": "de",
+    "Italià": "it",
+    "Portuguès": "pt",
+    "Neerlandès": "nl",
+    "ucrainès": "uk",
+    "Rus": "ru"
+}
