14f79e3d1dce91fa6b3f5b18432e38ea8f4d7605
Dunocat639YT <francescpasto@gmail.com>
2025-05-02 23:35:22 +0200

    .

---
+++ C/Course/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    // This is my first program in C! :)
+
+    /*
+    This
+    is
+    a
+    multi-line
+    comment
+    */
+    
+    printf("I like pizza!\n");
+    printf("It's really good!");
+
+    return 0;
+}
---
+++ C/Course/variables.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    // variable = Reusable container for a value.
+    //            Behaves as if it were the value it contains.
+    
+    int age = 25;
+    int year = 2025;
+    int quantity = 1;
+    float gpa = 2.5;
+    float price = 19.99;
+
+    printf("You are %d years old\n", age);
+    printf("The year is %d\n", year);
+    printf("You have ordered %d x items\n", quantity);
+    printf("Your gpa is %.1f\n", gpa);
+    printf("The price is %.1f€", price);
+
+    return 0;
+}
