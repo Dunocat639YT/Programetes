4a21b63e1f80bcba5b6e6136dda8f6de1b25cbf1
Dunocat639YT <francescpasto@gmail.com>
2025-05-15 23:25:07 +0200

    nigger counter

---
+++ Python/counter.py
@@ -0,0 +1,11 @@
+import time
+
+print("NIGGER COUNTER")
+print()
+number = input("How many niggers I'm about to count? ")
+number = int(number)
+numero = 0
+for i in range (number):
+    numero+=1
+    print(f"There are {numero} niggers")
+    time.sleep(0.1)
