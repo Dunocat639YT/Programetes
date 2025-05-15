b44973a6b10e057edffd41d4254692950f50b5dd
Dunocat639YT <francescpasto@gmail.com>
2025-05-15 23:33:25 +0200

    .

--- Python/counter.py
+++ Python/counter.py
@@ -1,11 +1,22 @@
 import time
+import os
 
-print("NIGGER COUNTER")
+RED = "\033[91m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+BLUE = "\033[94m"
+PURPLE = "\033[95m"
+RESET = "\033[0m"
+
+print(f"{YELLOW}WELCOME TO NIGGER COUNTER 2025{RESET}")
 print()
-number = input("How many niggers I'm about to count? ")
-number = int(number)
+niggers = input(f"{GREEN}How many niggers I'm about to count?{RESET} ")
+niggers = int(niggers)
 numero = 0
-for i in range (number):
+for i in range (niggers):
     numero+=1
-    print(f"There are {numero} niggers")
-    time.sleep(0.1)
+    print(f"{RED}There are {numero} niggers{RESET}")
+    print()
+    time.sleep(0.001)
+
+os.system("PAUSE")
