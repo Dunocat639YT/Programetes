d9082b1cf49f05c40a8e9309c16e0f8d6bdfc07f
Dunocat639YT <francescpasto@gmail.com>
2025-05-22 22:13:52 +0200

    .

--- C++/Course/logical-operators.cpp
+++ C++/Course/logical-operators.cpp
@@ -2,9 +2,29 @@
 
 int main(){
 
-    // && = check if two conditions are true
-    // || = check if at least onte of two conditiona is true
-    // !  = reverses the logical state of its operand
+    // && (and) = check if two conditions are true
+    // || (or)  = check if at least onte of two conditiona is true
+    // !  (not) = reverses the logical state of its operand
+
+    int temp;
+    bool sunny = true;
+
+    std::cout << "Enter the temperature: ";
+    std::cin >> temp;
+
+    if(temp <= 0 || temp >= 30){
+        std::cout << "The temperature is bad!\n";
+    }
+    else{
+        std::cout << "The temperature is good!\n";
+    }
+
+    if(!sunny){
+        std::cout << "It's cloudy outside!\n";
+    }
+    else{
+        std::cout << "It's sunny outside!\n";
+    }
 
 
     return 0;
