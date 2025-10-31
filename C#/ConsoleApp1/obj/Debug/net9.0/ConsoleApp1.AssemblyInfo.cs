70e3f0d7b7dd3c501cd31ebc27d0ec25c2c1bc93
Dunocat639YT <francescpasto@gmail.com>
2025-10-31 18:24:01 +0100

    .

--- C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1898013b2d0a31d22fdbfdbff016547c4319f81e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd52244bd7c4f9f68354762f8d5a47ef85299fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- C++/rock-paper-scissors.cpp
+++ C++/rock-paper-scissors.cpp
@@ -3,19 +3,18 @@ using namespace std;
 
 
 int main(){
-    string computer;
-    string player;
-    bool game = true;
-    string resposta;
+
+    string choice
+    
 
     string opcions[3] = {"pedra", "paper", "tisores"};
 
     cout << "************* Pedra, paper i tisores! *************" << '\n';
-    cout << "Quin és el teu pròxim moviment? ";
-    cin >> resposta;
-    if (resposta != "pedra" && != "paper" && != "tisores"){
-        cout << resposta << " no és una opció vàlida." << '\n';
-        cout << "Torna-ho a intentar: ";
+    
+    do{
+        cout << "Quin és el teu moviment?" << '\n'
+        cin >> choice
+        
     }
 
     
