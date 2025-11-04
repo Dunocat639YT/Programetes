f813f8952bd9ad52b66e57689e40e5e4b78b9c75
Dunocat639YT <francescpasto@gmail.com>
2025-11-04 14:02:02 +0100

    .

--- C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc7eeb9f9b7a32cd762269a8081685a4b8b2963")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b690545c5f7128ec761034b77a76c61ff801a819")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- C++/rock-paper-scissors.cpp
+++ C++/rock-paper-scissors.cpp
@@ -1,26 +1,92 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
+
+int maquina(int ordinador);
+int comprovar(int ordinador, string jugador);
 
 
 int main(){
 
-    string resposta;
-
-    string opcions[3] = {"pedra", "paper", "tisores"};
+    string jugador;
 
     cout << "************* Pedra, paper i tisores! *************" << '\n' << '\n';
     
     do{
         cout << "Quin és el teu moviment?" << '\n';
-        cin >> resposta;
+        cin >> jugador;
         
-    } while(resposta != "pedra" && resposta != "paper" && resposta != "tisores");
+    } while(jugador != "pedra" && jugador != "paper" && jugador != "tisores");
+
+    maquina();
+
+    comprovar();
 
     
     return 0;
 }
 
 
-string ordinador(){
-    return 0;
+int maquina(){
+    srand(time(0));
+    int ordinador = 1 + rand() % 3;
+
+    return ordinador;
+}
+
+int comprovar(int ordinador, string jugador){
+
+    switch (ordinador)
+    {
+        case 1:
+
+            cout << "La màquina ha escollit pedra..." << '\n' << '\n';
+
+            if (jugador == "pedra"){
+                cout << "Empat!" << '\n' << '\n' << '\n';
+            }
+            else if (jugador == "paper"){
+                cout << "Has guanyat!" << '\n' << '\n' << '\n';
+            }
+            else{
+                cout << "Has perdut!" << '\n' << '\n' << '\n';
+            }
+            
+            break;
+
+        case 2:
+
+            cout << "La màquina ha escollit paper..." << '\n' << '\n';
+
+
+            if (jugador == "pedra"){
+                cout << "Has perdut!" << '\n' << '\n' << '\n';
+            }
+            else if (jugador == "paper"){
+                cout << "Empat!" << '\n' << '\n' << '\n';
+            }
+            else{
+                cout << "Has guanyat!" << '\n' << '\n' << '\n';
+            }
+            break;
+
+        case 3:
+
+            cout << "La màquina ha escollit tisores..." << '\n' << '\n';
+
+
+            if (jugador == "pedra"){
+                cout << "Has guanyat!" << '\n' << '\n' << '\n';
+            }
+            else if (jugador == "paper"){
+                cout << "Has perdut!!" << '\n' << '\n' << '\n';
+            }
+            else{
+                cout << "Empat!" << '\n' << '\n' << '\n';
+            }
+            break;
+    }
+    
 }
