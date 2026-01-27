085bfeb1bad3fc5dfcc9a6ac6eaf5d6d61aa1682
Dunocat639YT <francescpasto@gmail.com>
2026-01-27 13:43:00 +0100

    .

---
+++ C++/temps.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "*************** El conversor de temps 3000 ***************";
+    
+    std::cout << "Selecciona una opcció:" << '\n';
+    std::cout << "1) Segons a minuts" << '\n';
+    std::cout << "2) Minuts a segons" << '\n';
+    std::cout << "3) Minuts a hores" << '\n';
+    std::cout << "4) Hores a minuts" << '\n';
+    std::cout << "Hores a dies" << '\n';
+    std::cout << "Dies a hores" << '\n';
+    std::cout << "Dies a setmanes" << '\n';
+    std::cout << "Setmanes a dies" << '\n';
+    std::cout << "Setmanes a anys " << '\n';
+    std::cout << "Dies a setmanes" << '\n';
+    std::cout << "Setmanes a anys" << '\n';
+    std::cout << "Dies a anys" << '\n';
+    std::cout << "Anys a dies" << '\n';
+
+    std::
+
+
+    return 0;
+}
