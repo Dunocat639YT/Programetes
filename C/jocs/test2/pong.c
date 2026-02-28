75b03d09087f858922f33fe472ce9cef4ff3196d
Dunocat639YT <francescpasto@gmail.com>
2026-02-28 01:41:28 +0100

    .

--- C/jocs/test2/pong.c
+++ C/jocs/test2/pong.c
@@ -1,8 +1,23 @@
 #include <raylib.h>
 
-void controls(int key, float *posició, float velocitat) {
+// Struct jugadors (inicialitazció)
+typedef struct {
+    Vector2 posició;
+    Vector2 mida;
+    const float velocitat;
+    } Jugador;
 
-    if(IsKeyDown(key)) *posició += velocitat;
+// Struct bola (inicialitazció)
+typedef struct {
+    Vector2 posició;
+    const float radi;
+    Vector2 velocitat;
+    Vector2 maxVel;
+    } Bola;
+
+void controls(int key, Jugador *c, float velocitat) {
+
+    if(IsKeyDown(key)) c->posició.y += velocitat * GetFrameTime();
 }
 
 int main(void) {
@@ -13,63 +28,80 @@ int main(void) {
 
     InitWindow(ampladaPantalla, alturaPantalla, títol);
 
-    SetTargetFPS(60);
+    SetTargetFPS(240);
 
-    typedef struct {
-        Vector2 posició = {ampladaPantalla / 10, alturaPantalla / 2};
-        Vector2 mida = {10, 80};
-        const float velocitat = 15};
-    } jugador1
+    // Struct jugador 1
+    Jugador j1 = {
+        .posició = {ampladaPantalla / 10, alturaPantalla / 2},
+        .mida = {10, 80},
+        .velocitat = 800
+    };
 
-    typedef struct jugador2 {
-        Vector2 posició = {ampladaPantalla / 10, alturaPantalla / 2};
-        Vector2 mida = {10, 80};
-        const float velocitat = 15};
-    } jugador2
+    // Struct jugador 2
+    Jugador j2 = {
+        .posició = {ampladaPantalla - (ampladaPantalla / 10), alturaPantalla / 2},
+        .mida = {10, 80},
+        .velocitat = 800
+    };
 
-    // Posició i mida jugador
-    Vector2 posicióJugador1 = {ampladaPantalla / 10, alturaPantalla / 2};
-    Vector2 posicióJugador2 = {ampladaPantalla - (ampladaPantalla / 10), alturaPantalla / 2};
-    Vector2 midaJugadors = {10, 80};
-
-    // Velocitat jugadors
-    const float velocitatJugadors = 15;
-
-    // Posició i velocitat de la bola
-    Vector2 posicióBola = {ampladaPantalla/2, alturaPantalla/2};
-    Vector2 velocitatBola = {5, 4};
-    const float radiBola = 30.0f;
+    // Struct bola
+    Bola bola = {
+        .posició = {ampladaPantalla/2, alturaPantalla/2},
+        .radi = 30.0f,
+        .velocitat = {300, 200},
+        .maxVel = {600, 400}
+    };
 
     while(!WindowShouldClose()){
 
-        // Controls
-        controls(KEY_W, &posicióJugador1.y, -velocitatJugadors);
-        controls(KEY_S, &posicióJugador1.y, velocitatJugadors);
-        controls(KEY_UP, &posicióJugador2.y, -velocitatJugadors);
-        controls(KEY_DOWN, &posicióJugador2.y, velocitatJugadors);
+        // Controls jugadors
+        controls(KEY_W, &j1, -j1.velocitat);
+        controls(KEY_S, &j1, j1.velocitat);
+        controls(KEY_UP, &j2, -j2.velocitat);
+        controls(KEY_DOWN, &j2, j2.velocitat);
 
         // Moviment bola
-        posicióBola.x += velocitatBola.x;
-        posicióBola.y += velocitatBola.y;
+        bola.posició.x += bola.velocitat.x * GetFrameTime();
+        bola.posició.y += bola.velocitat.y * GetFrameTime();
 
-        // Físiques bola
-        if (posicióBola.x >= (ampladaPantalla - radiBola) || posicióBola.x <= radiBola) {
-            velocitatBola.x *= -1.0f;
+        // Col·lisió bola amb parets
+        if (bola.posició.x >= (ampladaPantalla - bola.radi) || bola.posició.x <= bola.radi) { // Rebotar horitzontal
+        bola.velocitat.x *= -1.0f;
         }
-        if (posicióBola.y >= (alturaPantalla - radiBola) || posicióBola.y <= radiBola) {
-            velocitatBola.y *= -1.0f;
+        if (bola.posició.y >= (alturaPantalla - bola.radi) || bola.posició.y <= bola.radi) { // Rebotar verticalment
+            bola.velocitat.y *= -1.0f;
         }
+
+        // Limitar velocitat
+        if (bola.maxVel.x =< bola.velocitat.x) {
+            bola.velocitat.x = bola.maxVel.x
+
+        }
+
+
 
         BeginDrawing();
 
-        ClearBackground(BLACK);
+                ClearBackground(BLACK);
 
-        //Dibuixar bola
-        DrawCircleV(posicióBola, radiBola, WHITE);
+                //Dibuixar bola
+                DrawCircleV(bola.posició, bola.radi, WHITE);
 
-        // Dibuixar jugador 1 i 2
-        DrawRectangleV(posicióJugador1, midaJugadors, WHITE);
-        DrawRectangleV(posicióJugador2, midaJugadors, WHITE);
+                // Dibuixar jugador 1 i 2
+                DrawRectangleV(j1.posició, j1.mida, WHITE);
+                DrawRectangleV(j2.posició, j2.mida, WHITE);
+
+                // Col·lisió bola amb jugadors
+                if (CheckCollisionCircleRec(bola.posició, bola.radi, (Rectangle){j1.posició.x, j1.posició.y, j1.mida.x, j1.mida.y})){
+                    bola.velocitat.x *= -1.1f;
+                    bola.posició.x = j1.posició.x + j1.mida.x + bola.radi;
+                }
+                else if (CheckCollisionCircleRec(bola.posició, bola.radi, (Rectangle){j2.posició.x, j2.posició.y, j2.mida.x, j2.mida.y})){
+                    bola.velocitat.x *= -1.1f;
+                    bola.posició.x = j2.posició.x - bola.radi;
+                }
+
+
         
 
         EndDrawing();
